2806658f622152896c62c5e834ef8549f36d18bf
willdunklin <dunkliw@clarkson.edu>
2021-03-31 13:22:27 -0400

    Moved to readme

--- closest_factors.py
+++ closest_factors.py
@@ -1,9 +1,3 @@
-# %% 
-# Here we're trying to get the nearest factors of a number n
-# For example, we may want to convert a 1d array of length a
-# into the most square 2d matrix possible. This algorithm
-# efficiently shows what the dimensions a,b should be for n
-
 # %%
 from math import sqrt, ceil
 
